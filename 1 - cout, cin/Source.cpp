89f25022a783058fbdbee544f721750c501269c2
sny1411 <matteo2004@outlook.fr>
2021-01-31 18:54:21 +0100

    cin + cout

---
+++ 1 - cout, cin/Source.cpp
@@ -0,0 +1,7 @@
+#include <iostream> //pour pouvoir utiliser cout << ""
+#include <Windows.h> //pour pouvoir utiliser system()
+
+int main(){
+    std::cout << "Hello World !" << std::endl; //affiche hello world et fait un retour à la ligne avec endl
+    system("pause"); //met en pause cmd
+}
